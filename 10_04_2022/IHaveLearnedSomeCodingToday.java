26d5bf2b844891d1ad43ce11ff5f9ab92d9cf5d2
jrnuttallscode <joshuarnuttall@gmail.com>
2022-10-04 16:55:52 -0400

    commit

---
+++ 10_04_2022/IHaveLearnedSomeCodingToday.java
@@ -0,0 +1,33 @@
+// name of the method is print, display and show and post and each method should
+// have a print statment which will print life is beautiful ! on the screen.
+
+public class IHaveLearnedSomeCodingToday {
+    public static void main(String[] args) {
+
+        print();
+        display();
+        show();
+        post();
+    }
+
+    public static void print() {
+        String a = "Life";
+        System.out.println(a);
+    }
+
+    public static void display() {
+        String a = "is";
+        System.out.println(a);
+    }
+
+    public static void show() {
+        String a = "beautiful";
+        System.out.println(a);
+    }
+
+    public static void post() {
+        String a = "!";
+        System.out.println(a);
+    }
+
+}
